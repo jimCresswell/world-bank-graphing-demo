9569e8a47b8f006dc24a34c1b753f86aaf706674
jimCresswell <jim.cresswell@gmail.com>
2014-11-21 08:09:43 +0000

    Add stub for data processing module.

---
+++ src/javascript/service/processWorldBankIndicatorData.js
@@ -0,0 +1,17 @@
+/**
+ * The data from the World Bank for world growth indicators
+ * can be downloaded here
+ * http://data.worldbank.org/topic/financial-sector#tp_wdi .
+ *
+ * It can be downloaded in Excel, XML and CSV formats,
+ * this module is for transforming the CSV format data
+ * into a nested JavaScript object and array format.
+ */
+
+module.exports = processData;
+
+function processData(csvData) {
+    var data = {};
+
+    return data;
+}

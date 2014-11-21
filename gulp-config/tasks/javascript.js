29a14a28142072c133a0424cb3e89099959571f9
jimCresswell <jim.cresswell@gmail.com>
2014-11-21 14:07:07 +0000

    * Move JS service testing into its own task. * Run linting before testing. * Rename lint-javascript-blah to javascript-lint-blah

--- gulp-config/tasks/javascript.js
+++ gulp-config/tasks/javascript.js
@@ -16,33 +16,35 @@ var gutil = require('gulp-util');
 
 var config = require('../config').javascript;
 
-// Lint and fail the task if the linting fails.
-gulp.task('lint-javascript-client', function() {
+// Linting
+gulp.task('javascript-lint-client', function() {
     return gulp.src(config.srcClient)
         .pipe(jshint('.jshintrc'))
-        .pipe(jshint.reporter(stylish))
-        .pipe(jshint.reporter('fail'));
+        .pipe(jshint.reporter(stylish));
 });
-gulp.task('lint-javascript-service', function() {
+gulp.task('javascript-lint-service', function() {
     return gulp.src(config.srcClient)
         .pipe(jshint('.jshintrc'))
-        .pipe(jshint.reporter(stylish))
-        .pipe(jshint.reporter('fail'));
+        .pipe(jshint.reporter(stylish));
 });
 
-gulp.task('javascript-client', ['lint-javascript-client'], function() {
-    var bundleStream = browserify(config.clientEntryPoint)
+// Testing
+gulp.task('javascript-test-service', ['javascript-lint-service'], function() {
+    return gulp.src('')
+        .pipe(shell('jasmine-node --noStack  ./spec/service', {ignoreErrors: true}))
+        .on('error', gutil.log);
+});
+
+// Get a text stream from browserify,
+// pipe to vinyl-source-stream with an output
+// filename argument to create a buffer stream,
+// run any intermediate tasks then write to file.
+gulp.task('javascript-client', ['javascript-lint-client'], function() {
+    var bundleTextStream = browserify(config.clientEntryPoint)
         .bundle();
-
-    // Pass desired output filename to vinyl-source-stream,
-    // run any intermediate tasks then write to file.
-    return bundleStream
+    return bundleTextStream
         .pipe(source(config.clientBundleFilename))
         .pipe(gulp.dest(config.dest));
 });
 
-gulp.task('javascript-service', ['lint-javascript-service'], function() {
-    return gulp.src('')
-        .pipe(shell('jasmine-node --noStack  ./spec/', {ignoreErrors: true}))
-        .on('error', gutil.log);
-});
+gulp.task('javascript-service', ['javascript-lint-service', 'javascript-test-service']);

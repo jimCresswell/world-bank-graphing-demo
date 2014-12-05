8107046c395b65de28fc1845fbac8a3f3eabc57b
jimCresswell <jim.cresswell@gmail.com>
2014-12-05 12:44:46 +0000

    Actually add the tooltip module file.

---
+++ src/javascript/client/chart/chart-types/worldBankIndices/tooltip.js
@@ -0,0 +1,93 @@
+/**
+ * Tooltip functionality.
+ *
+ * To be mixed into the parent chart prototype.
+ */
+'use strict';
+
+var d3 = require('d3');
+var formatValuesFactory = require('./helpers').formatValuesFactory;
+
+/**
+ * Given a node append a tooltip to it.
+ *
+ * Note: can't append node after setting content
+ * because the content relies on the inherited
+ * data from the parent node and because the
+ * getBoundingClientRect method requires
+ * the element to be in the DOM.
+ *
+ * @param  {DOMNode} node
+ * @return {undefined}
+ */
+exports.appendTooltip = function(node) {
+    var chart = this;
+    var datapoint = d3.select(node);
+    var tooltip = datapoint.append('g');
+
+    tooltip
+        .classed('tooltip', true);
+
+
+    // Calculate the offset directions for
+    // tooltips according to which chart
+    // quadrant the datapoint is in.
+    // The default tooltip offset (in the
+    // top left quadrant) is down and to
+    // the right.
+    var xOffsetSign = 1;
+    var yOffsetSign = 1;
+    var textAnchor = 'start';
+    var plotWidth = chart.dimensions.width - chart.padding.left - chart.padding.right;
+    var plotHeight = chart.dimensions.height - chart.padding.top - chart.padding.bottom;
+    var translate = d3.transform(datapoint.attr('transform')).translate;
+    var xTranslate = translate[0];
+    var yTranslate = translate[1];
+    if (xTranslate >= plotWidth/2) {
+        xOffsetSign = -1;
+        textAnchor = 'end';
+    }
+    if (yTranslate >= plotHeight/2) {
+        yOffsetSign = -1;
+    }
+
+    // Append the region tooltip content.
+    tooltip.append('text')
+        .text(function(d) {return d.region;});
+
+    // Append the indices descriptors and values content
+    // with a vertical offset.
+    ['x','y','z'].forEach(function(dimension, i) {
+        var indexObject = chart.data.indices[chart.accessors[dimension]];
+        var descriptor = indexObject.descriptor;
+        var formatter = formatValuesFactory(indexObject.symbol);
+        tooltip.append('text')
+            .text(function(d) {return descriptor + ': ' + formatter(d[dimension]);})
+            .attr({
+               y: 20*(i+1)
+            });
+    });
+
+    // Position the tooltip according to
+    // its offset directions.
+    tooltip
+        .attr('transform', function(d) {
+            var circleRadius = chart.scales.z(d.z);
+
+            // Default, move the tooltip down a bit.
+            var yOffset = yOffsetSign * circleRadius * 0.5;
+
+            // Move the tooltip up.
+            if (yOffsetSign === -1) {
+                yOffset = yOffsetSign * (tooltip.node().getBoundingClientRect().height - circleRadius);
+            }
+
+            // Horizontal displacement, acts together with
+            // text-anchor: start/end.
+            // Offset by circle radius plus a constant.
+            var xOffset = xOffsetSign * (circleRadius + 4);
+
+            return 'translate(' + xOffset + ',' + yOffset + ')';
+        })
+        .style({'text-anchor': textAnchor});
+};
